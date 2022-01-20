74cef7777c4074f319207ea6b810d47f9dd047e5
Dayo Akinsola <dayoakinsola3@gmail.com>
2022-01-20 22:49:02 +0000

    removed unused icons

--- src/components/AlbumPage/TrackList.js
+++ src/components/AlbumPage/TrackList.js
@@ -1,7 +1,4 @@
 import { useEffect } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPauseCircle, faPlayCircle } from '@fortawesome/free-solid-svg-icons';
-
 
 const TrackList = ({ albumDetails, togglePlaying, setAlbumDetails, pauseTrack }) => {
 
