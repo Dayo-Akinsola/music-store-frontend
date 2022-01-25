a618f0190797d72ea5211511c2efca536e918076
Dayo Akinsola <dayoakinsola3@gmail.com>
2022-01-25 21:45:16 +0000

    removed unused functions

--- src/components/HomePage/Genre.js
+++ src/components/HomePage/Genre.js
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
-import firstLetterUpper from "./helpers/firstLetterUpper";
 
 const importGenreImages = (r) => {
   let images = {};
@@ -8,11 +7,10 @@ const importGenreImages = (r) => {
   return images;
 }
 const genreImages = importGenreImages(require.context('../../assets/genre-images', false, /\.(png|jpe?g|svg)$/));
-const Genre = ({ topAlbums, genre, bgColor, top}) => {
+const Genre = ({ genre, bgColor}) => {
 
   const [ attribution, setAttribution ] = useState(null);
 
-  console.log(genreImages);
   useEffect(() => {
     const setMessage = () => {
       if (genre === 'pop') {
@@ -54,52 +52,32 @@ const Genre = ({ topAlbums, genre, bgColor, top}) => {
     flexDirection: 'column',
     alignItems: 'center',
     margin: '1.5em auto',
-    // width: '28em',
-    // position: 'relative',
     marginBottom: '2em',
   }
   
   const imagesContainerStyle = {
     margin: 'auto',
-    //borderWidth: '2px 2px 2px 34px',
-    //borderStyle: 'solid',
     borderColor: `${bgColor}`,
-    //padding: '0.3em',
     backgroundColor: `${bgColor}`,
   }
   const imageStyle = {
-    width: '19em',
-    height: '19em',
+    width: '14em',
+    height: '14em',
+    filter: 'brightness(0.8)',
   }
-
-  const labelStyle = {
-    display: 'none',
-    fontWeight: 600,
-    fontFamily: 'Founders Grotesk, Arial Black, sans-serif',
-    fontSize: '1.2em',
-    position: 'absolute',
-    top: `${top}`,
-    left: '0.8em',
-    background: 'none',
-    transformOrigin: '0 0',
-    transform: 'rotate(270deg)',
-    marginLeft: '3.5em',
-    marginTop: '5em',
-    color: '#1D3447',
-  }
-
 
   return (
   <div className={`home__genres--${genre} genre`} style={genreContainer}  >
-    <span className={`home__genres--${genre}-label genre-label`} style={labelStyle}>{genre === 'hiphop' ? 'Hip Hop' : firstLetterUpper(genre)}</span>
-    <div className={`home__genres--${genre}-imgs genre-imgs`} style={imagesContainerStyle}>
-        <img src={genreImages[`${genre}.jpg`]} alt={genre} className={`home__genres--${genre}-img`} style={imageStyle} />
-    </div>
+    <Link to={genre === 'hiphop' ? '/shop/hip-hop' : `/shop/${genre}`}>
+      <div className={`home__genres--${genre}-imgs genre-imgs`} style={imagesContainerStyle}>
+          <img src={genreImages[`${genre}.jpg`]} alt={genre} className={`home__genres--${genre}-img`} style={imageStyle} />
+      </div>
+    </Link>
     <div className={`home__genres--${genre}-btn-container genre-btn-container`} >
       <Link className={`home__genres--${genre}-link genre-link`} to={genre === 'hiphop' ? '/shop/hip-hop' : `/shop/${genre}`}>
         <button className={`home__genres--${genre}-btn genre-btn`}>View {genre.toUpperCase()}</button>  
       </Link>
-      <span className={`home--genres--${genre}-img-credit`}>{attribution}</span>
+      <span className={`home__genres--${genre}-img-credit img-credit`}>{attribution}</span>
     </div>
   </div>
   )
--- src/components/HomePage/Genres.js
+++ src/components/HomePage/Genres.js
@@ -1,63 +1,21 @@
-import { useState, useEffect } from 'react';
 import Genre from './Genre';
 
-const Genres = ({ albums }) => {
-
-  const [topAlbums, setTopAlbums] = useState({
-    pop: [],
-    rock: [],
-    hiphop: [],
-    electronic: [],
-    jazz: [],
-  });
-
-  const getMostPopularAlbums = (genre) => {
-    if (genre !== undefined && genre !== []) {
-      const albumsPopularity = genre.map((album) => {
-        return {
-          title: album.title,
-          image: album.cover_image,
-          albumPopularity: album.community.want + album.community.have,
-          id: album.id,
-        }
-      });
-      const sortedByPopularity = albumsPopularity.sort((a, b) => b.albumPopularity - a.albumPopularity);
-      return sortedByPopularity.slice(0, 4);
-    }
-  }
-
-  useEffect(() => {
-    const { pop, rock, hiphop, electronic, jazz } = albums;
-    setTopAlbums({
-      pop: getMostPopularAlbums(pop),
-      rock: getMostPopularAlbums(rock),
-      hiphop: getMostPopularAlbums(hiphop),
-      electronic: getMostPopularAlbums(electronic),
-      jazz: getMostPopularAlbums(jazz),
-    });     
+const Genres = () => {
   
-  }, [albums]);
-
-  if (topAlbums.pop.length !== 0) {
-    
-    return(
-      <div className="home__genres">  
-        <div className="home__genres--title-wrapper home__sub-title-wrapper">
-          <span className="home__genres--title home__sub-title">Genres</span>
-        </div>
-        <div className="home__genres--content">
-          <Genre topAlbums={topAlbums} genre='pop' bgColor='#fae1dd' top='2.3em' />
-          <Genre topAlbums={topAlbums} genre='rock' bgColor='#cfbaf0' top='3em' />
-          <Genre topAlbums={topAlbums} genre='hiphop' bgColor='#bbdefb' top='3.9em' />
-          <Genre topAlbums={topAlbums} genre='electronic' bgColor='#fbf8cc' top='4.1em' />
-          <Genre topAlbums={topAlbums} genre='jazz' bgColor='#caffbf' top='3em' />
-        </div>
+  return(
+    <div className="home__genres">  
+      <div className="home__genres--title-wrapper home__sub-title-wrapper">
+        <span className="home__genres--title home__sub-title">Genres</span>
       </div>
-    )     
-  }
-
-  return null;
-
+      <div className="home__genres--content">
+        <Genre genre='pop' bgColor='#fae1dd'  />
+        <Genre genre='rock' bgColor='#cfbaf0' />
+        <Genre genre='hiphop' bgColor='#bbdefb'  />
+        <Genre genre='electronic' bgColor='#fbf8cc'  />
+        <Genre genre='jazz' bgColor='#caffbf' />
+      </div>
+    </div>
+  )     
   
 }
 
