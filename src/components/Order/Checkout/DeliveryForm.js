e546346921e23332b6bd233d1320038dda9f780e
Dayo Akinsola <dayoakinsola3@gmail.com>
2022-02-14 22:17:04 +0000

    renamed Details to DeliveryForm

--- src/components/Order/Checkout/DeliveryForm.js
+++ src/components/Order/Checkout/DeliveryForm.js
@@ -1,17 +1,10 @@
-import { useState } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { getRequest ,dataChangeRequest } from '../../../sevices/service';
 
-const Details = () => {
-
-  const [ formDetails, setFormDetails ] = useState({
-    firstName: '', 
-    lastName: '', 
-    address: '', 
-    city: '', 
-    postCode: '', 
-    phone: '', 
-    email: '',
-  });
+const DeliveryForm = ({ user, deliveryDetails, setDeliveryDetails }) => {
+  
   const [ errorMessages, setErrorMessages ] = useState({
     firstName: '',
     lastName: '', 
@@ -22,12 +15,25 @@ const Details = () => {
     email: '',
   });
 
+  useEffect(() => {
+    const setStoredDetails = async () => {
+      const { token } = user;
+      if (token) {
+        const response = await getRequest('http://localhost:3001/users/details', token);
+        const userDetails = await response.json();
+        setDeliveryDetails(userDetails);
+      }
+    }
+
+    setStoredDetails();
+  }, [user]);
+
   const navigate = useNavigate();
 
   const updateForm = (name, value) => {
-    const updatedFormDetails = {...formDetails};
+    const updatedFormDetails = {...deliveryDetails};
     updatedFormDetails[name] = value;
-    setFormDetails(updatedFormDetails);
+    setDeliveryDetails(updatedFormDetails);
   }
 
   const handleInputChange = (event) => {
@@ -51,7 +57,7 @@ const Details = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const inputNames = Object.keys(formDetails);
+    const inputNames = Object.keys(deliveryDetails);
     let isFormValid = false;
     const formattedNames = {
       firstName: 'First Name',
@@ -66,7 +72,7 @@ const Details = () => {
     inputNames.forEach(name => {
       let errorCaught = false;
       if (name === 'postCode') {
-        const postCodeCheck = /^([A-Z]{1,2}\d[A-Z\d]? ?\d[A-Z]{2}|GIR ?0A{2})$/.test(formDetails.postCode);
+        const postCodeCheck = /^([A-Z]{1,2}\d[A-Z\d]? ?\d[A-Z]{2}|GIR ?0A{2})$/.test(deliveryDetails.postCode);
         const invalidPostCodeMessage = 'You must enter a valid UK Post Code';
         if (!postCodeCheck) {
           setErrorMessages(messages => ({...messages, [name]: invalidPostCodeMessage}));
@@ -75,7 +81,7 @@ const Details = () => {
       }
 
       if (name === 'phone') {
-        const value = formDetails[name];
+        const value = deliveryDetails[name];
         const invalidNumberMessage = 'You must enter a valid UK Phone Number.';
         if (value.length !== 10 && value.length !== 11) {
           setErrorMessages(messages => ({...messages, [name]: invalidNumberMessage}));
@@ -84,7 +90,7 @@ const Details = () => {
       }
 
       if (name === 'email') {
-        const emailCheck = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formDetails.email);
+        const emailCheck = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(deliveryDetails.email);
         const invalidEmailMessage = 'You must enter a valid Email Address';
         if (!emailCheck) {
           setErrorMessages(messages => ({...messages, [name]: invalidEmailMessage}));
@@ -92,7 +98,7 @@ const Details = () => {
         }
       }
 
-      if (formDetails[name].length === 0) {
+      if (deliveryDetails[name].length === 0) {
         const errorMessage = `Please enter your ${formattedNames[name]}.`;
         setErrorMessages((messages) => ({...messages, [name]: errorMessage}));
         errorCaught = true;
@@ -109,12 +115,13 @@ const Details = () => {
     });
 
     if (isFormValid) {
+      dataChangeRequest('http://localhost:3001/users/details', deliveryDetails, user.token, 'PUT');
       navigate('/payment');
     }
   }
 
   const autoFillForm = () => {
-    setFormDetails({
+    setDeliveryDetails({
       firstName: 'Dayo',
       lastName: 'Akinsola',
       address: '123 Random Street',
@@ -134,14 +141,14 @@ const Details = () => {
         <div className="form__name">
           <div className="form__first-name form-section">
             <label htmlFor="firstName" className="form__first-name--label">First Name</label>
-            <input type="text" className="form__first-name--input" name="firstName" onChange={handleInputChange} value={formDetails.firstName} />
+            <input type="text" className="form__first-name--input" name="firstName" onChange={handleInputChange} value={deliveryDetails.firstName} />
             <div className="form__first-name--error">
               <span className="form__first-name--error-message error-message">{errorMessages.firstName}</span>
             </div>
           </div>
           <div className="form__last-name form-section">
             <label htmlFor="lastName" className="form__last-name--label">Last Name</label>
-            <input type="text" className="form__last-name--input" name="lastName" onChange={handleInputChange} value={formDetails.lastName}  />
+            <input type="text" className="form__last-name--input" name="lastName" onChange={handleInputChange} value={deliveryDetails.lastName}  />
             <div className="form__last-name--error">
               <span className="form__last-name--error-message error-message">{errorMessages.lastName}</span>
             </div>
@@ -149,35 +156,35 @@ const Details = () => {
         </div>
         <div className="form__city form-section">
           <label htmlFor="city" className="form__city--label">City / Town</label>
-          <input type="text" className="form__city--input" name="city" onChange={handleInputChange} value={formDetails.city}/>
+          <input type="text" className="form__city--input" name="city" onChange={handleInputChange} value={deliveryDetails.city}/>
           <div className="form__city--error">
               <span className="form__city--error-message error-message">{errorMessages.city}</span>
             </div>
         </div>
         <div className="form__address form-section">
           <label htmlFor="address" className="form__address--label">Address</label>
-          <input type="text" className="form__address--input" name="address" onChange={handleInputChange} value={formDetails.address} />
+          <input type="text" className="form__address--input" name="address" onChange={handleInputChange} value={deliveryDetails.address} />
           <div className="form__address--error">
             <span className="form__address--error-message error-message">{errorMessages.address}</span>
           </div>
         </div>
         <div className="form__post-code form-section">
           <label htmlFor="postCode" className="form__post-code--label">Post Code</label>
-          <input type="text" className="form__post-code--input" name="postCode" onChange={handleInputChange} value={formDetails.postCode} />
+          <input type="text" className="form__post-code--input" name="postCode" onChange={handleInputChange} value={deliveryDetails.postCode} />
           <div className="form__post-code--error">
               <span className="form__post-code--error-message error-message">{errorMessages.postCode}</span>
             </div>
         </div>
         <div className="form__phone form-section">
           <label htmlFor="phone" className="form__phone--label">Phone</label>
-          <input type="number" className="form__phone--input" name="phone" onChange={handleInputChange} value={formDetails.phone} />
+          <input type="number" className="form__phone--input" name="phone" onChange={handleInputChange} value={deliveryDetails.phone} />
           <div className="form__phone--error">
             <span className="form__phone--error-message error-message">{errorMessages.phone}</span>
           </div>
         </div>
         <div className="form__email form-section">
           <label htmlFor="email" className="form__email--label">Email Address</label>
-          <input type="email" className="form__email--input" name="email" onChange={handleInputChange} value={formDetails.email}  />
+          <input type="email" className="form__email--input" name="email" onChange={handleInputChange} value={deliveryDetails.email}  />
           <div className="form__email--error">
             <span className="form__email--error-message error-message">{errorMessages.email}</span>
           </div>
@@ -190,4 +197,4 @@ const Details = () => {
   )
 }
 
-export default Details;
+export default DeliveryForm;
