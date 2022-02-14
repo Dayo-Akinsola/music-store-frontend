9442b34b1453985b7309e6def5af256b365e5fec
Dayo Akinsola <dayoakinsola3@gmail.com>
2022-02-14 22:19:53 +0000

    passed in user and delivery details to delivery form

--- src/components/Order/Checkout/CheckoutPage.js
+++ src/components/Order/Checkout/CheckoutPage.js
@@ -1,11 +1,11 @@
-import Details from "./Details";
+import DeliveryForm from "./DeliveryForm";
 import CheckoutSummary from "./CheckoutSummary";
 
-const CheckoutPage = ({cart}) => {
+const CheckoutPage = ({ cart, user, deliveryDetails, setDeliveryDetails }) => {
   return (
     <div className="checkout">
       <h3 className="checkout__heading">Checkout</h3>
-      <Details  />
+      <DeliveryForm user={user} deliveryDetails={deliveryDetails} setDeliveryDetails={setDeliveryDetails}/>
       <CheckoutSummary cart={cart} />
     </div>
   )
