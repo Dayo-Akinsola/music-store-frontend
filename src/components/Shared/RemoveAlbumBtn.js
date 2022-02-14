d9fb7faa0b9b2cb717d70cad5fc7e79e45090268
Dayo Akinsola <dayoakinsola3@gmail.com>
2022-02-14 22:21:36 +0000

    removedCartAlbum function now only takes album id

--- src/components/Shared/RemoveAlbumBtn.js
+++ src/components/Shared/RemoveAlbumBtn.js
@@ -3,7 +3,7 @@ import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
 const RemoveAlbumBtn = ({ removeCartAlbum, cartAlbum, classNamePrefix }) => {
   return (
-    <span onClick={() => removeCartAlbum(cartAlbum)} className={`${classNamePrefix}--remove`}>
+    <span onClick={() => removeCartAlbum(cartAlbum.id)} className={`${classNamePrefix}--remove`}>
       <FontAwesomeIcon icon={faTimesCircle} />
     </span>
   )
