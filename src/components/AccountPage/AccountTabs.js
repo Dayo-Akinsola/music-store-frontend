d727cf5f0bdc25a4b341cafb8e45abde45586f6b
Dayo Akinsola <dayoakinsola3@gmail.com>
2022-02-16 23:21:48 +0000

    changed all the links to be relative

--- src/components/AccountPage/AccountTabs.js
+++ src/components/AccountPage/AccountTabs.js
@@ -4,9 +4,9 @@ const AccountTabs = () => {
   return (
     <div className="account__tabs">
       <AccountTab tabName="My Orders" className='orders' link='orders' /> 
-      <AccountTab tabName="My Wishlist" className='wishlist' link='/wishlist' /> 
-      <AccountTab tabName="My Details" className='details' link='/details' />  
-      <AccountTab tabName="My Friends" className='friends' link='/friends' /> 
+      <AccountTab tabName="My Wishlist" className='wishlist' link='wishlist' /> 
+      <AccountTab tabName="My Details" className='details' link='details' />  
+      <AccountTab tabName="My Friends" className='friends' link='friends' /> 
     </div>
   )
 }
