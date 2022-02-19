2564abb57317ceed25b4ef68963ee98473e5d837
Dayo Akinsola <dayoakinsola3@gmail.com>
2022-02-19 23:47:00 +0000

    created requests route

--- src/components/AccountPage/AccountViews/Friends/AccountFriends.js
+++ src/components/AccountPage/AccountViews/Friends/AccountFriends.js
@@ -7,7 +7,7 @@ const AccountFriends = () => {
         <h3 className="account__friends--heading">My Friends</h3>
         <nav className="account__friends--nav-bar">
           <NavLink to='friendlist' className="account__friends--nav-friendlist account__friends--nav-link">Friend List</NavLink>
-          <NavLink to='' className="account__friends--nav-requests account__friends--nav-link">Requests</NavLink>
+          <NavLink to='requests' className="account__friends--nav-requests account__friends--nav-link">Requests</NavLink>
         </nav>
       </div>
       <Outlet />
