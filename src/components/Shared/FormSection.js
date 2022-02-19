12caab2cbb596f2973416f0826d4607838130269
Dayo Akinsola <dayoakinsola3@gmail.com>
2022-02-19 23:49:15 +0000

    error message element is not rendered if the error message passed as a prop is null

--- src/components/Shared/FormSection.js
+++ src/components/Shared/FormSection.js
@@ -3,9 +3,14 @@ const FormSection = ({ name, className, label, detail, errorMessage, handleInput
     <div className={`form__${className} form-section`}>
       <label htmlFor={name} className={`form__${className}--label`}>{label}</label>
       <input type={type} className={`form__${className}--input`} name={name} onChange={handleInputChange} value={detail} />
-      <div className={`form__${className}--error`}>
-        <span className={`form__${className}--error-message error-message`}>{errorMessage}</span>
-      </div>
+      {
+        errorMessage ?
+          <div className={`form__${className}--error`}>
+            <span className={`form__${className}--error-message error-message`}>{errorMessage}</span>
+          </div>
+          :
+          null
+      }
     </div>
   )
 }
