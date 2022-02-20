bfb58e4d63111e7f638c3e39d5bd181c3c12f701
Dayo Akinsola <dayoakinsola3@gmail.com>
2022-02-20 18:14:12 +0000

    created component and added styles for friend list

--- src/components/AccountPage/AccountViews/Friends/Friendlist/AccountFriendList.js
+++ src/components/AccountPage/AccountViews/Friends/Friendlist/AccountFriendList.js
@@ -1,11 +1,37 @@
+import { useState, useEffect } from 'react';
+import { getRequest } from '../../../../../sevices/service';
 import AccountFriendListHeader from './AccountFriendListHeader';
 
 const AccountFriendList = ({ user }) => {
 
+  const [friends, setFriends] = useState([]);
+  useEffect(() => {
+    const getAllFriends = async () => {
+      const response = await getRequest('http://localhost:3001/friends/friendlist', user.token);
+      const allFriends = await response.json();
+      setFriends(allFriends);
+    }
+    getAllFriends();
+  }, [user.token]);
+
   return (
-    <div className="account__friends--friend-list">
+    <div className="account__friends--friend-list-container">
       <AccountFriendListHeader user={user} />
-      
+      <div className="account__friends--friend-list">
+        {
+          friends.map(friend => (
+            <div key={friend.id} className="account__friend-list--friend">
+              <div className="account__friend-list--profile-pic">
+                <span className="account__friend-list--profile-pic-text">{friend.name.slice(0, 2)}</span>
+              </div>
+              <div className="account__friend-list--details">
+                <span className="account__friend-list--name">{friend.name}</span>
+                <button className="account__friend-list--profile-btn">View Profile</button>
+              </div>
+            </div>
+          ))
+        }
+      </div>
     </div>   
   )
 }
