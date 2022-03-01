b016679c1e3090b14f4019979f688ea636695d22
Dayo Akinsola <dayoakinsola3@gmail.com>
2022-03-01 22:55:50 +0000

    Removed genres heading. Added popular albums heading

--- src/components/HomePage/Genres.js
+++ src/components/HomePage/Genres.js
@@ -4,9 +4,6 @@ const Genres = () => {
   
   return(
     <div className="home__genres">  
-      <div className="home__genres--title-wrapper home__sub-title-wrapper">
-        <span className="home__genres--title home__sub-title">Genres</span>
-      </div>
       <div className="home__genres--content">
         <Genre genre='pop'  />
         <Genre genre='rock' />
--- src/components/HomePage/PopularAlbums.js
+++ src/components/HomePage/PopularAlbums.js
@@ -36,35 +36,40 @@ const PopularAlbums = ({ runnersUp }) => {
   if (runnersUp) {
     return (
       <div className="home__popular-albums">
-        <div className="home__popular-albums--carousel-wrapper">
-          <div ref={carousel} className="home__popular-albums--carousel" style={carouselTransform}>
-            {runnersUp.map((album) => (
-              <Link key={album.id} to={`/shop/${encodeURIComponent(album.title).replaceAll('%20', '-').replace('---', '-')}/${album.type}/${album.id}`}>
-                <div className="home__popular-albums--album" >
-                  <div className="home__popular-albums--img-wrapper">
-                    <img src={album.image} alt={album.title} className="home__popular-albums--img" />
-                  </div>
-                  <div className="home__popular-albums--info">
-                    <h5 className="home__popular-albums--title">{album.title}</h5>
-                    <span className="home__popular-albums--artist">{album.artist}</span>
-                  </div>
-                </div>
-              </Link>
-            ))}
-          </div>
+        <div className="home__popular-albums--heading-wrapper">
+          <h4 className="home__popular-albums--heading">Popular Albums</h4>
         </div>
-        <Link to="/shop/popular">
-          <div className="home__popular-albums--btn-wrapper">
-            <button className="home__popular-albums--btn">Top Albums</button>
+        <div className="home__popular-albums--content">
+          <div className="home__popular-albums--carousel-wrapper">
+            <div ref={carousel} className="home__popular-albums--carousel" style={carouselTransform}>
+              {runnersUp.map((album) => (
+                <Link key={album.id} to={`/shop/${encodeURIComponent(album.title).replaceAll('%20', '-').replace('---', '-')}/${album.type}/${album.id}`}>
+                  <div className="home__popular-albums--album" >
+                    <div className="home__popular-albums--img-wrapper">
+                      <img src={album.image} alt={album.title} className="home__popular-albums--img" />
+                    </div>
+                    <div className="home__popular-albums--info">
+                      <h5 className="home__popular-albums--title">{album.title}</h5>
+                      <span className="home__popular-albums--artist">{album.artist}</span>
+                    </div>
+                  </div>
+                </Link>
+              ))}
+            </div>
           </div>
-        </Link>
-        <div className="home__popular-albums--nav-arrows">
-          <span className="home__popular-albums--left-arrow" onClick={decrementCounter} >
-            <FontAwesomeIcon icon={faAngleLeft} />
-          </span>
-          <span className="home__popular-albums--right-arrow" onClick={incrementCounter}>
-            <FontAwesomeIcon icon={faAngleRight}  />
-          </span>
+          <Link to="/shop/popular" className="home__popular-albums--link">
+            <div className="home__popular-albums--btn-wrapper">
+              <button className="home__popular-albums--btn">Top Albums</button>
+            </div>
+          </Link>
+          <div className="home__popular-albums--nav-arrows">
+            <span className="home__popular-albums--left-arrow" onClick={decrementCounter} >
+              <FontAwesomeIcon icon={faAngleLeft} />
+            </span>
+            <span className="home__popular-albums--right-arrow" onClick={incrementCounter}>
+              <FontAwesomeIcon icon={faAngleRight}  />
+            </span>
+          </div>
         </div>
       </div>
     )
