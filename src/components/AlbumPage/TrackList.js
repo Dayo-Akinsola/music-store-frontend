dfd2697d11f73fd78d021ba54d7bf110bd71d645
Dayo Akinsola <dayoakinsola3@gmail.com>
2022-03-05 23:09:01 +0000

    added styling for album details page and made it responsive

--- src/components/AlbumPage/TrackList.js
+++ src/components/AlbumPage/TrackList.js
@@ -26,9 +26,9 @@ const TrackList = ({ albumDetails, togglePlaying, setAlbumDetails, pauseTrack })
 
   return (
     <div className="album-page__extra--tracks-wrapper">
-      <h3 className="album-page__extra--tracks-heading">Album Tracks</h3>
-      {albumDetails.tracklist.map((track) => (
-        <div className="album-page__extra--track-wrapper" key={track.id}>
+      <h3 className="album-page__extra--tracks-heading">Tracklist</h3>
+      {albumDetails.tracklist.map((track, index) => (
+        <div className={`album-page__extra--track-wrapper${index % 2 === 0 ? ' coloured' : ''}`} key={track.id}>
           <div className="album-page__extra--track-details">
             <span className="album-page__extra--track-name"><b>Track:</b> {track.name}</span>
             <div className="album-page__extra--track-artists">
