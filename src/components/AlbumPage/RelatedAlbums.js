eeb2ce02986d2451a0ae87de9bc5103a5598becc
Dayo Akinsola <dayoakinsola3@gmail.com>
2022-03-05 23:06:17 +0000

    changed component to be a carousel

--- src/components/AlbumPage/RelatedAlbums.js
+++ src/components/AlbumPage/RelatedAlbums.js
@@ -1,52 +1,69 @@
-import { useState ,useEffect } from 'react';
+import { useState ,useEffect, useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleRight, faAngleLeft } from '@fortawesome/free-solid-svg-icons';
 import Album from '../ShopPage/Album';
 
-
 const RelatedAlbums = ({  albumDetails }) => {
 
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [displayedAlbums, setDisplayedAlbums] = useState([]);
+  const [slidePageCount, setSlidePageCount] = useState(7);
+  const [slidePage, setSlidePage] = useState(0);
+  const [slideTransform, setSlideTransform] = useState({});
+  const slide = useRef(null);
+
+
+  const incrementCounter = () => setSlidePage((slidePage + 1) % slidePageCount);
+  const decrementCounter = () =>  setSlidePage((((slidePage - 1) % slidePageCount) + slidePageCount) % slidePageCount);
+
 
   useEffect(() => {
-    const setShownAlbums = () => {
-      const nextAlbums = [];
-      albumDetails.relatedAlbums.map((album, index) => {
-        if (index === currentIndex || index === currentIndex + 1) {
-          nextAlbums.push(album);
-        }
-        return album;
-      });
-      setDisplayedAlbums(nextAlbums);
+    const setSlidePages = () => {
+      if (window.innerWidth < 900) {
+        setSlidePageCount(10);
+      } else if (window.innerWidth < 1200) {
+        setSlidePageCount(7);
+      } else if (window.innerWidth < 1400) {
+        setSlidePageCount(5)
+      } else {
+        setSlidePageCount(4);
+      }
     }
-
-    setShownAlbums();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currentIndex]);
-
-  const relatedLength = (albumDetails.relatedAlbums.relatedLength % 2 === 0) ? albumDetails.relatedAlbums.length : albumDetails.relatedAlbums.length - 1;
-  const moveDisplayForward = () => {
-    setCurrentIndex((currentIndex + 2) % relatedLength);    
-  }
-
-  const moveDisplayBackward = () => {
-    const newCurrentIndex = currentIndex - 2;
-    setCurrentIndex(((newCurrentIndex % relatedLength) + relatedLength) % relatedLength);
-  }
+    const moveDisplay = () => {
+      const slideWidth = slide.current.clientWidth;
+      const imageWidth = slide.current.childNodes[0].childNodes[0].clientWidth;
+      if (slidePage === slidePageCount - 1 && window.innerWidth >= 900 && window.innerWidth < 1200) {
+        /* Stops there being empty space at the end of the slide when there are three albums showing at a time */
+        setSlideTransform({
+          transform: `translateX( ${-slideWidth * (slidePage - 1) - (imageWidth * 2)}px)`,
+        })
+      } else {
+        setSlideTransform({
+        transform:  `translateX( ${-slideWidth * (slidePage)}px)`,
+        });
+      }
+     
+    }
+    setSlidePages();
+    moveDisplay();
+  }, [slidePageCount, slidePage]);
 
   return (
     <div className="album-page__extra--related-wrapper">
       <h3 className="album-page__extra--related-heading">Albums You Might Like</h3>
-      <div className="album-page__extra--related-albums">
-        {displayedAlbums.map(album => <Album album={album} key={album.id}/>  )}
+      <div className="album-page__extra--related-albums" ref={slide}>
+        <div className="album-page__extra--related-albums-inner" style={slideTransform}>
+          {albumDetails.relatedAlbums.map(album => <Album album={album} key={album.id}/>  )}
+        </div>
       </div>
       <div className="album-page__extra--nav-arrows">
         <span className="album-page__extra--left-arrow">
-          <FontAwesomeIcon icon={faAngleLeft} onClick={moveDisplayBackward} />
+          <FontAwesomeIcon icon={faAngleLeft} onClick={decrementCounter}/>
         </span>
+        <div className="album-page__related--page-count">
+          <span className="album-page__related--current-count">{slidePage + 1}</span>
+          <span className="album-page__related--total-count">{slidePageCount}</span>
+        </div>
         <span className="album-page__extra--right-arrow">
-          <FontAwesomeIcon icon={faAngleRight} onClick={moveDisplayForward} />
+          <FontAwesomeIcon icon={faAngleRight} onClick={incrementCounter}/>
         </span>
       </div>
     </div>
